74eb3d89786ea90b7a9ff7f2df7c70239a7edc84
Katharina01099 <yes>
2019-01-11 16:53:55 +0000

    form created

---
+++ js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded'), () => {
+  const newItemform = document.querySelector('new-item-form');
+  newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
+});
+
+const handleNewItemFormSubmit = function (event) {
+  event.preventDefault();
+
+  const horseListItem = createHorseListItem(event.target);
+  const horseListe = document.querySelector('#horse-list');
+  horseListe.appendChild(horseListItem);
+
+  event.target.reset();
+}
+
+const createHorseListItem = function (form) {
+  const horseListItem = document.createElement('li');
+  horseListItem.classList.add('horse-list-item');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  horseListItem.appendChild(name);
+
+  const breed = document.createElement('h2');
+  breed.textContent = form.breed.value;
+  horseListItem.appendChild(breed);
+
+  const stable = document.createElement('h2');
+  stable.textContent = form.stable.value;
+  horseListItem.appendChild(stable);
+
+  const color = document.createElement('p');
+  color.textContent = form.color.value;
+
+  return horseListItem;
+}
+
+
+};
